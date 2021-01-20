5e18f132e54e05f234eca75bb6c91694753ed8d2
Oleh Shevchuk <papalom31@gmail.com>
2021-01-20 16:07:32 +0200

    added Input in product

--- src/app/product/component/product/product.component.ts
+++ src/app/product/component/product/product.component.ts
@@ -1,19 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ProductModel } from '../../model/product-model';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.sass']
 })
-export class ProductComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-  onBuy(): void {
-      console.log('Product was bought ');
-  }
-
+export class ProductComponent  {
+  @Input()
+  product!: ProductModel;
 }
